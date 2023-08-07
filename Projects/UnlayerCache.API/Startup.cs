5d149286a65237313b384a524a67431f10a12a71
n.bitounis <n.bitounis@xe.gr>
2023-08-07 17:45:51 +0300

    * Added Swagger. * Added health check endpoint. * Updated docker file to a more realistic usage scenario. * Updated most libraries to latest-and-greatest.

--- Projects/UnlayerCache.API/Startup.cs
+++ Projects/UnlayerCache.API/Startup.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Amazon.DynamoDBv2;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -5,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using UnlayerCache.API.Services;
 
 namespace UnlayerCache.API;
@@ -21,7 +25,21 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
-        var options = Configuration.GetAWSOptions();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+	        options.SwaggerDoc("v1", new OpenApiInfo
+	        {
+		        Version = "v1",
+		        Title = "Unlayer Cache",
+		        Description = "A caching proxy for some methods of the Unlayer API."
+			});
+
+	        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename), true);
+        });
+		services.AddHealthChecks();
+		var options = Configuration.GetAWSOptions();
         services.AddScoped(_ => options.CreateServiceClient<IAmazonDynamoDB>());
         services.AddScoped<IDynamoService, DynamoService>();
         services.AddScoped<IUnlayerService, UnlayerService>();
@@ -29,7 +47,10 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
+	    app.UseSwagger();
+	    app.UseSwaggerUI();
+
+		if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
         }
@@ -45,8 +66,10 @@ public class Startup
             endpoints.MapControllers();
             endpoints.MapGet("/", async context =>
             {
-                await context.Response.WriteAsync("Unlayer API");
+                await context.Response.WriteAsync("Unlayer Cache API");
             });
         });
-    }
+
+        app.UseHealthChecks("/health");
+	}
 }
